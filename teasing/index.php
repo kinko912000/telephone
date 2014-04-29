21332de4a3c031bd78852f1ef83861a54385a2b5
yoshiaki <kinko912000@gmail.com>
2014-04-29 11:18:20 +0900

    make databases;

--- teasing/index.php
+++ teasing/index.php
@@ -1,13 +1,33 @@
-<html>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<title>MySQLへの接続テスト</title>
+</head>
 <body>
 
-<?php echo "top"; ?>
+<?php
 
-<form action = "./register.php" method = "post" >
-    <input type="submit" value = "start call ">
-</form >
+$dsn = 'mysql:dbname=test;host=localhost';
+$user = 'yoyaku';
+$password = 'yoyaku';
 
+try{
+    $dbh = new PDO($dsn, $user, $password);
 
+    $sql = 'select * from test';
+    foreach ($dbh->query($sql) as $row) {
+        print($row['name'].'<br />');
+    }
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+}
 
+$dbh = null;
+
+?>
+
+</body>
 </html>
