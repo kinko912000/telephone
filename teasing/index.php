4b66ad3e4e9e3441eaa017b7cdd07891e54c459c
yoshiaki <kinko912000@gmail.com>
2014-05-02 08:40:06 +0900

    twitter

--- teasing/index.php
+++ teasing/index.php
@@ -17,8 +17,7 @@
 
 <a href="./mail.php">メールを送信</a>
 
-twitterbutton
-<a href="http://twitter.com/intent/tweet?original_referer=http://ec2-54-178-190-97.ap-northeast-1.compute.amazonaws.com/index.php&url=http://ec2-54-178-190-97.ap-northeast-1.compute.amazonaws.com&text=SmartYoyaku">twitterでつぶやく</a><br>
-<a href="http://twitter.com/intent/tweet?text=UTF-8%E3%82%A8%E3%83%B3%E3%82%B3%E3%83%BC%E3%83%89%E3%81%97%E3%81%9F%E6%96%87%E5%AD%97%E3%82%92%E6%B8%A1%E3%81%99%E3%81%AE%E3%81%A7%E3%81%99&amp;via=446r">今すぐTwitterでつぶやく</a>
+Twitter<br>
+<a href="http://twitter.com/intent/tweet?original_referer=http://ec2-54-178-190-97.ap-northeast-1.compute.amazonaws.com/index.php&url=http://ec2-54-178-190-97.ap-northeast-1.compute.amazonaws.com&text=&amp;SmartYoyaku::">twitterでつぶやく</a><br>
 </body>
 </html>
